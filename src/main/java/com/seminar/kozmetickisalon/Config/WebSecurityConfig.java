ec88843ddc75cf57ea4272175abae18ecf86b389
teacurkovic <teacurkovic13@gmail.com>
2022-08-29 17:02:59 +0200

    Fix!

--- src/main/java/com/seminar/kozmetickisalon/Config/WebSecurityConfig.java
+++ src/main/java/com/seminar/kozmetickisalon/Config/WebSecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 import com.seminar.kozmetickisalon.Service.UserService;
 
@@ -21,7 +21,6 @@ import javax.annotation.Resource;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Resource
@@ -41,21 +40,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return authProvider;
     }
 
-
-  
-
+    
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
+        
         http
                 .authorizeRequests()
                     .antMatchers(
                             "/",
-                            "/registration**",
-                            "/static/**", 
+                            "/registration**", 
                             "/js/**",
                             "/css/**",
-                            "/image/**",
+                            "/images/**",
                             "/webjars/**").permitAll()
                 .and()
                     .formLogin()
@@ -71,8 +68,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                         .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                         .logoutSuccessUrl("/login?logout")
                         .permitAll();
-    
-        
+        http    .authorizeRequests()
+                    .antMatchers("/admin/*").access("hasRole('ADMIN')")
+                    .antMatchers("/role/*").access("hasRole('ADMIN')")
+                    .antMatchers("/user/*").access("hasRole('ADMIN')")
+                    .antMatchers("/reservations/*").access("hasRole('ADMIN')")
+                    .antMatchers("/offer/*").access("hasRole('ADMIN')")
+                    .antMatchers("/employee/*").access("hasRole('ADMIN')")
+                    .antMatchers("/welcome/*").access("hasRole('USER')")
+                
+                ;
+                
+                
     }
 
     @Bean
--- src/main/java/com/seminar/kozmetickisalon/Controller/HomeController.java
+++ src/main/java/com/seminar/kozmetickisalon/Controller/HomeController.java
@@ -1,58 +1,19 @@
 package com.seminar.kozmetickisalon.Controller;
 
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-import com.seminar.kozmetickisalon.Model.User;
-import com.seminar.kozmetickisalon.Service.UserService;
 @Controller
 public class HomeController {
-    @Autowired
-    UserService userService;
 
     @GetMapping("/login")
     public String login(Model model) {
         return "login";
     }
-    
-    @GetMapping("/welcome")
-    public String home(Model model) {
-        return "homepagee";
-    }
-    @ModelAttribute("userLogin")
-    public User userLogin() {
-        return new User();
-    }
 
-    @PostMapping("/process-login")
-    public ModelAndView Homepage(@ModelAttribute User userLogin, BindingResult result) {
-        ModelAndView mv = new ModelAndView("login");
-        if (result.hasErrors()){
-            return mv;
-        }
-        
-        User existing = userService.findByEmail(userLogin.getEmail());
-        if (existing == null){
-            mv.addObject("message", "User doesnt exist!");
-            return mv;
-        }else{
-            if(!userService.checkPassword(userLogin.getPassword(),existing)){
-                mv.addObject("message", "Password disnt correct!");
-                return mv;
-            }
-        }
-
-        mv.setViewName("homepagee");
-        return mv;
+    @GetMapping("/")
+    public String Homepage(Model model) {
+        return "Homepage";
     }
 
     @GetMapping("/ponuda")
